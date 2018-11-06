bf02f834fe6314a71bc37513c4aca04cf60098c1
sirbucristian <sirbucristi95@gmail.com>
2018-11-06 17:09:01 +0300

    lesson-9-js

---
+++ expl.js
@@ -0,0 +1,25 @@
+var a = 5;
+console.log(typeof a,a);
+var b = "String";
+console.log(typeof b,b);
+var c = null;
+console.log(typeof c,c);
+var d;
+console.log(typeof d,d);
+var user = { name: 'Name' };
+console.log (typeof user);
+var e = true;
+f = false;
+console.log (typeof e,e);
+
+var age = prompt('How old are you');
+if (age > 25) {
+  console.log(age, 'I am over 25 years old');
+} else if ( age = 25) {
+  console.log(age, 'I am 25');
+} else {
+  console.log(age, 'I am under 25');
+}
+ 
+var a = 23;
+(a >= 25) ?  console.log(a, 'I am over 25 years old') : console.log(a, 'I am under 25');
